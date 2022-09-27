3fc035f47980c412d481f48c8aebdf8b81cdbc73
tamaskis <tamas.a.kis@outlook.com>
2022-09-26 23:00:41 -0700

    updated function to new formatting

--- derivative.m
+++ derivative.m
@@ -6,19 +6,15 @@
 %   dy = derivative(x,y)
 %   dy = derivative(x,y,x_star)
 %
-% See also diff, gradient, iderivative, derivest.
+% See also diff, gradient.
 %
 % Copyright © 2021 Tamas Kis
-% Last Update: 2021-08-27
+% Last Update: 2022-09-26
 % Website: https://tamaskis.github.io
 % Contact: tamas.a.kis@outlook.com
 %
 % TECHNICAL DOCUMENTATION:
-% https://tamaskis.github.io/documentation/Basic_Numerical_Calculus.pdf
-%
-% REFERENCES:
-%   [1] http://www.ohiouniversityfaculty.com/youngt/IntNumMeth/lecture27.pdf
-%   [2] https://en.wikipedia.org/wiki/Finite_difference_method
+% https://tamaskis.github.io/files/Basic_Numerical_Calculus.pdf
 %
 %--------------------------------------------------------------------------
 %
@@ -48,19 +44,19 @@
 %
 %==========================================================================
 function [dy,x] = derivative(x,y,x_star)
-
+    
     % number of subintervals
     N = length(x)-1;
-
+    
     % preallocates vector to store derivative
     dy = zeros(size(x));
-
+    
     % approximates derivative at lower bound using forward difference
     dy(1) = (y(2)-y(1))/(x(2)-x(1));
-
+    
     % approximates derivative at upper bound using backward difference
     dy(N+1) = (y(N+1)-y(N))/(x(N+1)-x(N));
-
+    
     % approximates derivatives at all other nodes using central differences
     for i = 2:N
         dy(i) = (y(i+1)-y(i-1))/(x(i+1)-x(i-1));
@@ -70,5 +66,5 @@ function [dy,x] = derivative(x,y,x_star)
     if nargin == 3
         dy = interp1(x,dy,x_star,'linear','extrap');
     end
-        
+    
 end
